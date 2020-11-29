2863203f5906ffe8a1ac770b5cec366b15ee2067
nick repasy <nrepasy@gmail.com>
2020-11-29 22:24:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,7 +13,7 @@ array_1 = [
 row_index = 0
 while row_index < array_1.count do
   element_index = 0
-  while element_index < array_1[row_index] do
+  while element_index < array_1[row_index].min do
     p array_1[row_index][element_index]
     element_index += 1
   end
